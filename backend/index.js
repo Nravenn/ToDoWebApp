d0bfb89182083cd35e9ce0ae62789d01b7ac8749
Nomi <n.naomiraven@gmail.com>
2026-04-30 19:06:13 +0800

    update 30042026

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,16 @@ const app = express(); // Using Express.js to power app
 //-----------------Middleware setup--------------
 app.use(express.json());
 
-app.use(cors("*"));
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || origin.endsWith(".vercel.app")) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
 
 // MONGO SETUP
 (async () => {
--- frontend/js/api.js
+++ frontend/js/api.js
@@ -2,7 +2,7 @@
 const taskForm = document.getElementById('taskForm');
 const toDoList = document.getElementById('toDoList');
 const completedList = document.getElementById('completedList');
-const url = "http://localhost:3000";
+const url = "https://todowebapp-5760.onrender.com";
 
 
 // general functions that can be used throughout this program or call on multiple times
